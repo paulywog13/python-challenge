c96d964a7a0827a6e59d99e4d699415646a5e176
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-09-27 15:11:26 -0600

    Adding main.py for each file

---
+++ PyBank/main.py
@@ -0,0 +1,5 @@
+import os
+import csv
+
+pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
+
---
+++ PyPoll/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+
+pypoll_csv = os.path.join("..", "Resources", "election_data.csv")
+
+print(pypoll_csv)
