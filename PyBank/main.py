5ff3c2d6656f697772808f9672379b889f52a413
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-09-27 15:44:43 -0600

    Checking that the path is correct

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,4 +2,3 @@ import os
 import csv
 
 pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
-
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,4 +3,3 @@ import csv
 
 pypoll_csv = os.path.join("..", "Resources", "election_data.csv")
 
-print(pypoll_csv)
