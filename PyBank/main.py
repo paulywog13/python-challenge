a921cdfb78ee826cb990b0a540fc630f0d7d2f42
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-09-29 23:01:13 -0600

    Coding added to both main.py files for both HW challenges.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,39 @@
 import os
 import csv
 
-pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
+pybank_csv = os.path.join("Resources", "budget_data.csv")
+
+
+
+
+month_count = 0
+
+def revenue_row
+
+
+with open(pybank_csv) as pybankcsv:
+    pybankreader = csv.reader(pybankcsv, delimiter = ',')
+    csv_header = next(pybankcsv)
+    total_revenue = 0
+    average_change = 0
+    i = 1
+    for row in pybankreader:
+        month_count += 1
+        total_revenue += int(row[1])
+        if row[0]== "Revenue"
+        
+        #average_change = 
+
+    
+
+
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total months: {month_count}")
+print(f"Total: ${total_revenue}")
+#print(pybankreader)
+#print(total_change)
+#print(f"Average Change: {average_change}")
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,48 @@
+#import library
+
 import os
 import csv
 
-pypoll_csv = os.path.join("..", "Resources", "election_data.csv")
+election_csv = os.path.join("Resources", "election_data.csv")
 
+#def election_results(election_data)
+
+
+
+with open(election_csv, newline="") as election_file:
+    election_reader = csv.reader(election_file, delimiter= ",")
+    election_header = next(election_file)
+
+    votes =[]
+    total_votes = 0
+    candidate_votes = []
+
+    candidates_dict = {}
+
+    #candidate name 
+
+    for candidate in election_reader:
+
+        votes.append(candidate[0])
+        total_votes = (len(votes))
+        candidate = (candidate[2])
+        if candidate in candidates_dict:
+            candidates_dict[candidate]+=1 #Creating a count in the dictionary
+        else:
+            candidates_dict = 1 #Candidate in Dictionary
+
+
+
+
+
+
+
+
+
+
+
+#print("------------------------------")
+#print("    Election Results:")
+#print("------------------------------")
+#print(f"Total Votes: {total_votes}")
+#print(f"")
