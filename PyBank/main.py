22cad8b5268e57ef5c5df3375418e6d2b4ae349c
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-09-30 22:32:59 -0600

    Added summing formula in loop function

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,26 +3,23 @@ import csv
 
 pybank_csv = os.path.join("Resources", "budget_data.csv")
 
-
-
-
 month_count = 0
-
-def revenue_row
-
+revenue_changes = []
 
 with open(pybank_csv) as pybankcsv:
     pybankreader = csv.reader(pybankcsv, delimiter = ',')
     csv_header = next(pybankcsv)
     total_revenue = 0
     average_change = 0
-    i = 1
+    previous_revenue = 867884
     for row in pybankreader:
         month_count += 1
         total_revenue += int(row[1])
-        if row[0]== "Revenue"
-        
-        #average_change = 
+        total_revenue_change = int(row[1])-previous_revenue
+        revenue_changes.append(total_revenue_change)
+        previous_revenue = int(row[1])
+    print(sum(revenue_changes)/len(revenue_changes))
+         
 
     
 
@@ -33,7 +30,7 @@ print("----------------------------")
 print(f"Total months: {month_count}")
 print(f"Total: ${total_revenue}")
 #print(pybankreader)
-#print(total_change)
+#print(total_revenue_change)
 #print(f"Average Change: {average_change}")
 
 
