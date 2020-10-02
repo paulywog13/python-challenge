6ab502e502320e24211a75009d7dd25361d00ca3
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-10-01 23:18:40 -0600

    Completed Bank Assignment and added code to Poll Assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,9 @@ pybank_csv = os.path.join("Resources", "budget_data.csv")
 
 month_count = 0
 revenue_changes = []
+months = []
 
+outputfile= os.path.join('Analysis', "Budget_Data.txt")
 with open(pybank_csv) as pybankcsv:
     pybankreader = csv.reader(pybankcsv, delimiter = ',')
     csv_header = next(pybankcsv)
@@ -13,24 +15,35 @@ with open(pybank_csv) as pybankcsv:
     average_change = 0
     previous_revenue = 867884
     for row in pybankreader:
+        
         month_count += 1
         total_revenue += int(row[1])
+        months.append(row[0])
         total_revenue_change = int(row[1])-previous_revenue
         revenue_changes.append(total_revenue_change)
         previous_revenue = int(row[1])
-    print(sum(revenue_changes)/len(revenue_changes))
-         
-
+        greatest_increase = max(revenue_changes)
+        greatest_decrease = min(revenue_changes)
+                
+    average_change = (round(sum(revenue_changes)/(len(revenue_changes)-1), 2))
+    print(greatest_increase)
+    print(greatest_decrease)
+    max_month = months[revenue_changes.index(greatest_increase)]    
+    min_month = months[revenue_changes.index(greatest_decrease)]
     
 
 
+output =(
+f"Financial Analysis\n"
+f"----------------------------\n"
+f"Total months: {month_count}\n"
+f"Total: ${total_revenue}\n"
+f"Average Change: ${average_change}\n"
+f"Greatest Increase in Profits: {max_month} (${greatest_increase})\n"
+f"Greatest Increase in Profits: {min_month} (${greatest_decrease})\n"
+)
+print(output)
 
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total months: {month_count}")
-print(f"Total: ${total_revenue}")
-#print(pybankreader)
-#print(total_revenue_change)
-#print(f"Average Change: {average_change}")
-
+with open(outputfile, "w") as txt_file:
+    txt_file.write(output)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,34 +5,67 @@ import csv
 
 election_csv = os.path.join("Resources", "election_data.csv")
 
-#def election_results(election_data)
-
-
-
+outputfile= os.path.join('Analysis', "Polling_Data.txt")
 with open(election_csv, newline="") as election_file:
     election_reader = csv.reader(election_file, delimiter= ",")
     election_header = next(election_file)
 
-    votes =[]
     total_votes = 0
-    candidate_votes = []
-
+    candidate_name = {}
+    candidates_votes = []
+    winning_candidate=[]
     candidates_dict = {}
+    most_votes = 0
 
     #candidate name 
 
-    for candidate in election_reader:
-
-        votes.append(candidate[0])
-        total_votes = (len(votes))
-        candidate = (candidate[2])
+    for candidates in election_reader:
+        total_votes += 1
+        candidates_votes.append(candidates[0])
+        candidate = (candidates[2])
         if candidate in candidates_dict:
-            candidates_dict[candidate]+=1 #Creating a count in the dictionary
+            candidates_dict[candidate]+=1
         else:
-            candidates_dict = 1 #Candidate in Dictionary
+            candidates_dict[candidate]=1 
+
+    #cand1_perc_vote = ("{:.2%}".format(candidates_dict[0]/total_votes))
+    #cand2_perc_vote = ("{:.2%}".format(candidates_dict[1]/total_votes))
+    #cand3_perc_vote = ("{:.2%}".format(candidates_dict[2]/total_votes))
+    #cand4_perc_vote = ("{:.2%}".format(candidates_dict[3]/total_votes))
+        
+    print(candidates_dict.items())
+    #print(candidates_dict.get(0))
+    for candidate, candidates_votes in candidates_dict.items():
+
+        percent_of_vote = ("{:.2%}".format(candidates_votes/total_votes))
+        
+        #print(f"{candidate}: {percent_of_vote} ({candidates_votes})\n")
+        if candidates_votes > most_votes:
+            most_votes = candidates_votes
+            candidates = candidate
+     
 
 
+    output =(
+    f"------------------------------\n"
+    f"    Election Results:\n"
+    f"------------------------------\n"
+    f"Total Votes: {total_votes}\n"
+    f"------------------------------\n"
+    #f"{candidate}: {cand1_perc_vote} ({candidates_dict[0]})\n"
+    #f"{candidate}: {cand2_perc_vote} ({candidates_dict[1]})\n"
+    #f"{candidate}: {cand3_perc_vote} ({candidates_dict[2]})\n"
+    #f"{candidate}: {cand4_perc_vote} ({candidates_dict[3]})\n"
+    f"------------------------------\n"
+    f"WINNER:  {candidates}\n"
+    )
 
+    print(output)
+
+    #with open(outputfile, "w") as txt_file:
+        #txt_file.write(output)    
+        
+    
 
 
 
