ce9045a569b65ea7581a6383eb9227873c92818b
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-10-03 14:36:55 -0600

    Descriptions added to main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,28 @@
+#import library
+
 import os
 import csv
 
+#define input path in order to read and ananlyze data
 pybank_csv = os.path.join("Resources", "budget_data.csv")
 
+#define variables for analysis including lists to use for to analyze data
 month_count = 0
 revenue_changes = []
 months = []
 
+#define output path for analysis data
 outputfile= os.path.join('Analysis', "Budget_Data.txt")
 with open(pybank_csv) as pybankcsv:
+    #skip the header line
     pybankreader = csv.reader(pybankcsv, delimiter = ',')
     csv_header = next(pybankcsv)
     total_revenue = 0
     average_change = 0
     previous_revenue = 867884
+
+    #for loop to sum number of months, create a list of months, create a list of revenue values for each month, and 
+    #to find the maximum and minimum revenus values from the data
     for row in pybankreader:
         
         month_count += 1
@@ -25,14 +34,16 @@ with open(pybank_csv) as pybankcsv:
         greatest_increase = max(revenue_changes)
         greatest_decrease = min(revenue_changes)
                 
+    #average change calulation based on the for loop data. Subract one from list of revenue_changes as extra one added for starting value
     average_change = (round(sum(revenue_changes)/(len(revenue_changes)-1), 2))
     print(greatest_increase)
     print(greatest_decrease)
+    #identify month of greatest and least amount of revenue indexing the values from our revenue list to find the matching month value
     max_month = months[revenue_changes.index(greatest_increase)]    
     min_month = months[revenue_changes.index(greatest_decrease)]
     
 
-
+#output file to write analysis results to text file in analysis folder
 output =(
 f"Financial Analysis\n"
 f"----------------------------\n"
@@ -42,8 +53,10 @@ f"Average Change: ${average_change}\n"
 f"Greatest Increase in Profits: {max_month} (${greatest_increase})\n"
 f"Greatest Increase in Profits: {min_month} (${greatest_decrease})\n"
 )
-print(output)
 
 with open(outputfile, "w") as txt_file:
     txt_file.write(output)
+
+#print analysis to terminal
+print(output)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,20 +3,25 @@
 import os
 import csv
 
+#define input path in order to read and ananlyze data
 election_csv = os.path.join("Resources", "election_data.csv")
 
+#define output path for analysis data
 outputfile= os.path.join('Analysis', "Polling_Data.txt")
 with open(election_csv, newline="") as election_file:
     election_reader = csv.reader(election_file, delimiter= ",")
+    #skip the header line
     election_header = next(election_file)
-
+    
+    #define variables for analysis including lists and a candidate dictionary to recall values later
     total_votes = 0
     candidates_votes = []
     winning_candidate=[]
     candidates_dict = {}
     most_votes = 0
 
-     
+    #for loop to tally total votes 
+    #Dictionary included in for loop to store each candidate's name and their total number of votes    
 
     for candidates in election_reader:
         total_votes += 1
@@ -27,49 +32,39 @@ with open(election_csv, newline="") as election_file:
         else:
             candidates_dict[candidate]=1 
 
-   
+   #for loop using the dictionary key(candidate) and values(candidate_votes) to identify the winning candidate
     
     for candidate, candidates_votes in candidates_dict.items():
         
         if candidates_votes > most_votes:
             most_votes = candidates_votes
             candidates = candidate
-     
+
+    #output file to write analysis results to text file
+
     with open(outputfile, "w") as txt_file:
     
         output =(
         f"------------------------------\n"
-        f"    Election Results:\n"
+        f"      Election Results:       \n"
         f"------------------------------\n"
-        f"Total Votes: {total_votes}\n"
+        f"Total Votes: {total_votes}    \n"
         f"------------------------------\n"
         )
         print(output)
         txt_file.write(output)
+        
+        #for loop to for each candidate in order to generate their vote percentage and vote total based upon the total number of votes
+        
         for candidate, candidates_votes in candidates_dict.items():
+            
+            #vote percentage calculation and output format for the the results
             percent_of_vote = ("{:.2%}".format(candidates_votes/total_votes))
             txt_file.write(f"{candidate}: {percent_of_vote} ({candidates_votes})\n")
-            print(f"{candidate}: {percent_of_vote} ({candidates_votes})\n")
+            print(f"{candidate}: {percent_of_vote} ({candidates_votes})")
         txt_file.write(f"------------------------------\n")
         print(f"------------------------------")
         txt_file.write(f"WINNER:  {candidates}\n")
         print(f"WINNER:  {candidates}")   
 
     
-
-        
-        
-    
-
-
-
-
-
-
-
-
-#print("------------------------------")
-#print("    Election Results:")
-#print("------------------------------")
-#print(f"Total Votes: {total_votes}")
-#print(f"")
